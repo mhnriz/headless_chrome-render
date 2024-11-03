e5412140ca86fd17f9bbb34b9290c915bf3d9bc4
Hariz <49497959+mhnriz@users.noreply.github.com>
2024-11-03 21:24:03 +0800

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -2,8 +2,5 @@
 #!/usr/bin/env bash
 set -x
 
-# Add Chrome's official repository to install the stable version
-apt update && apt install -y wget gnupg
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
-apt update && apt install -y google-chrome-stable
+# Update package list and install Chromium
+apt update && apt install -y chromium-browser
